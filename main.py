b7cfe50e26c8d7e51ebfb74bada5ce03bdc8de4f
b9010840 <g.a.blenkinsop1@newcastle.ac.uk>
2020-09-29 18:28:54 +0100

    Introduce basic regression calculation using age and players dribbled past as the dependent variables

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-import sklearn
+from sklearn import linear_model
 import pandas as pd
 import numpy as np
 
 
 dataset = 'data.csv'
 data = pd.read_csv(dataset)
+ind_var = data[['Dispos']]
+dep_var = data[['Age', '#Pl']]
 
+regr = linear_model.LinearRegression()
+regr.fit(dep_var, ind_var)
 
