c0394d85b96345872ee596639edb9c069654a86b
b9010840 <g.a.blenkinsop1@newcastle.ac.uk>
2020-10-09 17:48:12 +0100

    Complete menu_print in menu.py

--- main.py
+++ main.py
@@ -2,35 +2,66 @@ from sklearn import linear_model
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
-from menu import menu_print
+import menu
 
 variables = ['Age', 'Def 3rd', 'Mid 3rd', 'Att 3rd', '#Pl', 'Megs', 'PrgDist', 'Rec']
 dataset = 'data.csv'
+filters = []
 
-
-
-
-x =sm.add_constant(dep_var)
-model = sm.OLS(ind_var,x).fit()
-pred = model.predict(x)
-print(model.summary())
 
 def datasetchanger():
-    menu_print()
+    global dataset
+    dataset = input("=============DATA=============" +
+                    "\nPlease enter the relative location and file name:\n"
+                    )
+    menu.menu_print()
+
 
 def filterchanger():
-    menu_print()
+    global filters
+    filter_var = input("============FILTER============" +
+                       "\nPlease enter the filtered variable from the following:" +
+                       "\n" + ''.join(variables)
+                       )
+    if filter_var in variables:
+        filter_eq = input("\nPlease enter the equation for" + filter_var + "below as follows (== 6, > 23, etc):\n")
+        filter = filter_var + " " + filter_eq
+        filters.append(filter)
+        menu.menu_print()
+    else:
+        print("===========WARNING============" +
+              "\nInvalid inputted variable"
+              )
+        menu.menu_print()
 
-def runregression():
-    data = pd.read_csv(dataset)
+
+def variablechanger():
+    global variables
+    menu.menu_print()
+
+
+def runregression(d, v, f):
+    data = pd.read_csv(d)
     ind_var = data[['Dispos']]
-    dep_var = data[variables]
+    dep_var = data[v]
+    filters = f
+
+    x = sm.add_constant(dep_var)
+    model = sm.OLS(ind_var, x).fit()
+    pred = model.predict(x)
+    print(model.summary())
+
 
 def printfilters():
-    print(filters)
+    return ''.join(filters)
+
 
 def printdataset():
-    print(dataset)
+    return ''.join(dataset)
+
 
 def printvariables():
-    print(variables)
+    return ' - '.join(variables)
+
+
+menu.menu_print()
--- menu.py
+++ menu.py
@@ -1,20 +1,25 @@
-from main import datasetchanger, filterchanger, runregression, printdataset, printfilters
-
-
 def menu_print():
-    choice = input("=============DATA=============" +
-                   "\nCurrent Dataset:\n" + printdataset() +
-                   "\nCurrent Filters:\n" + printfilters() +
-                   "\n\n=============MENU=============" +
-                   "\n1 - Change Dataset" +
-                   "\n2 - Change Filters" +
-                   "\n3 - Run Regression Analysis"
-                   "\n\n============CHOICE============" +
-                   "\nInput - "
-                   )
+    from main import printdataset, printfilters, printvariables
+    ds = printdataset()
+    fi = printfilters()
+    va = printvariables()
+    print("=============DATA=============\nCurrent Dataset:\n" + ds +
+          "\nCurrent Filters:\n" + fi +
+          "\nCurrent Variables:\n" + va +
+          "\n\n=============MENU=============\n1 - Change Dataset\n2 - Change Filters\n3 - Change Variables\n4 - Run "
+          "Regression Analysis\n")
+
+    choice = input("\n============CHOICE============ \nInput - ")
+
     if choice == 1:
+        from main import datasetchanger
         datasetchanger()
     if choice == 2:
+        from main import filterchanger
         filterchanger()
     if choice == 3:
+        from main import variablechanger
+        variablechanger()
+    if choice == 4:
+        from main import runregression
         runregression()
